d9c1c7db5ba5aef3e5d4c3becb358a42e2362f06
jarivankaam <jari@redbanana.nl>
2023-01-26 14:03:46 +0100

    added breeze

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Solitreo&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+        <script defer src="{{ asset('/js/app.js') }}"></script>
+        <!-- Scripts -->
+    </head>
+<body>
+<div class="section-header flex justify-content-center align-items-center" id="myHeader">
+        <ul class="flex justify-content-around align-items-center">
+            <li><a  href="/">Home</a></li>
+            <li><h2 class="font Size64 family title">ImageApi</h2></li>
+            <li><a href="/dashboard">dashboard</a></li>
+        </ul>
+</div>
+<div class="content">
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur et fugit nobis saepe temporibus ut.
+        Blanditiis commodi quia tenetur ullam? Asperiores deleniti excepturi ipsum iusto non qui velit. Ducimus, ipsum.
+    </div>
+    <div>Accusamus, aliquam at aut dolor eligendi facilis, fugiat hic laudantium, magnam molestias nihil numquam odio
+        quas quibusdam sequi unde vero vitae. Blanditiis cumque delectus harum nesciunt voluptates. Cupiditate,
+        incidunt, pariatur?
+    </div>
+    <div>Aliquid aspernatur dicta dolorem eligendi facilis harum ipsa ipsam labore, odio officia perspiciatis quisquam,
+        saepe, soluta veritatis voluptas! A assumenda at dolorem non! A consectetur cum excepturi magni minima nulla.
+    </div>
+    <div>A adipisci animi architecto cupiditate debitis est eum fuga ipsam laborum magnam necessitatibus nesciunt nihil
+        non, quaerat quia quis, rem reprehenderit sit, tempore temporibus ullam vel voluptate! Consectetur dicta, ipsum?
+    </div>
+    <div>Animi ducimus, iure modi praesentium repellat repudiandae sequi voluptatem? Animi commodi facilis fugit
+        molestiae nulla similique, voluptatem! Aspernatur dolore ducimus ea, eligendi eos expedita id minima molestias
+        suscipit, ut voluptatem!
+    </div>
+    <div>Fugit mollitia obcaecati repudiandae velit? Aliquam distinctio, error excepturi facere fugit illo, incidunt nam
+        non officia officiis pariatur qui quo repudiandae velit voluptatem. Adipisci ducimus necessitatibus officia
+        pariatur, perspiciatis veritatis.
+    </div>
+    <div>Alias dolores explicabo harum illum, magnam minus nihil officia officiis perspiciatis quaerat quam qui quis
+        recusandae repellat repudiandae similique voluptatibus. Alias at deserunt fuga harum iste iusto maiores quia
+        ratione.
+    </div>
+    <div>Dolore facere laboriosam laborum recusandae ut! Asperiores commodi cum ea, ipsam minima neque perferendis quas
+        voluptas. Commodi ea laborum minus neque quas quia quod ratione rem similique sunt, tenetur ut.
+    </div>
+    <div>Adipisci alias amet architecto aspernatur beatae corporis cum dolorem facere fugiat, incidunt iste, nostrum,
+        nulla temporibus. Adipisci distinctio doloremque, expedita molestiae placeat, quaerat quibusdam quod, recusandae
+        rerum saepe sed soluta?
+    </div>
+    <div>Alias aliquid amet animi consequatur cum dolor dolorem dolorum explicabo, fugit nemo nulla odio, provident qui
+        recusandae, sequi soluta ullam velit vitae! Assumenda consequuntur eaque est illo impedit minima qui?
+    </div>
+    <div>Doloremque dolorum itaque iusto? Aliquam animi atque, culpa cum dolorum error inventore laborum maxime nesciunt
+        nulla numquam ratione suscipit. Alias culpa dolore itaque labore molestias qui quibusdam sed sint voluptates.
+    </div>
+    <div>Adipisci architecto aspernatur incidunt minima non nulla numquam placeat qui quibusdam. Ab aliquid atque
+        consectetur deserunt dicta id magni minus molestias odio, officiis optio recusandae rem temporibus totam unde
+        voluptatum!
+    </div>
+    <div>A ab aliquid consequuntur debitis dignissimos distinctio dolores, et excepturi exercitationem explicabo facere
+        harum hic molestias necessitatibus neque obcaecati officiis perspiciatis quae quas reiciendis repellendus
+        reprehenderit sit vel vero voluptas.
+    </div>
+    <div>Deserunt illum magni odit provident repudiandae, veritatis. A alias aliquid animi distinctio et eveniet
+        inventore iste iure labore, vero! Commodi, dignissimos doloribus ducimus harum in natus officiis quae qui
+        suscipit?
+    </div>
+    <div>Aliquid animi, asperiores consequuntur culpa cum cumque exercitationem inventore itaque, laudantium pariatur
+        possimus qui sed sit, tempora vitae! Dolorem eum ex fugit ipsam qui repudiandae sunt voluptate? Facilis
+        quibusdam, ullam?
+    </div>
+    <div>Accusantium asperiores aspernatur at blanditiis corporis dolorem doloremque ea error incidunt ipsa itaque
+        laborum modi officiis reprehenderit rerum tempore tenetur ullam, ut veniam vero? Itaque maxime minus pariatur
+        veniam vero.
+    </div>
+    <div>Aliquid, animi doloribus ducimus eius eos, ex exercitationem facere molestiae optio quo repellendus suscipit
+        vel voluptatibus? Atque beatae esse illo incidunt iure modi omnis rerum voluptates! Error exercitationem
+        officiis vel.
+    </div>
+    <div>Aliquam animi asperiores aspernatur culpa dolore ducimus, ea ipsa iure laboriosam minima modi mollitia nemo
+        neque, officia perferendis porro reprehenderit sapiente temporibus unde vitae. Ad fugit ipsa possimus
+        reprehenderit temporibus!
+    </div>
+    <div>A alias asperiores atque, aut culpa, deserunt distinctio, eius eligendi esse eum eveniet fuga illo impedit ipsa
+        ipsam laborum libero molestiae optio perferendis quae quasi quidem quis repellat vero voluptate!
+    </div>
+    <div>A alias aspernatur atque culpa cum delectus dolorum earum ex fuga illo iusto, labore laboriosam, libero nihil
+        odit perferendis quae qui quisquam ratione reprehenderit repudiandae sed sit sunt tempore unde.
+    </div>
+    <div>Accusamus consequatur dolorum est non quas qui rem. Aliquid, asperiores consectetur consequatur culpa cumque,
+        ea eum exercitationem id impedit libero molestias necessitatibus obcaecati omnis quae quis reprehenderit saepe
+        suscipit velit?
+    </div>
+    <div>Aliquam amet animi, architecto, corporis dicta dolore doloremque dolores exercitationem facere magni natus
+        necessitatibus nostrum officiis omnis quo quos tempora vero? At atque commodi cupiditate exercitationem id neque
+        officiis quis?
+    </div>
+    <div>Ad adipisci aliquam commodi consequatur delectus deleniti dolorem eos explicabo ipsam labore odit officia porro
+        recusandae, sint unde. Consectetur inventore soluta sunt tempora. Consequuntur dicta dolorum esse ipsam
+        laudantium perspiciatis!
+    </div>
+    <div>Accusantium alias animi commodi dolore eius hic illum iure magnam maxime molestias, nam perferendis provident
+        quas quibusdam quo rerum veniam? Alias architecto blanditiis, distinctio eligendi rem repellat sit tempore
+        veniam.
+    </div>
+    <div>Assumenda culpa enim id minus odit quis quisquam similique voluptatibus! A deserunt ipsum labore nisi quae quam
+        quidem sit. Amet blanditiis dolorem doloremque expedita fugit magnam qui quo ratione unde!
+    </div>
+    <div>Aspernatur cumque, ducimus eius nostrum numquam reiciendis tempore temporibus! Asperiores aspernatur debitis
+        deleniti ducimus enim error eveniet facere fuga illo impedit, non numquam odio porro, praesentium quibusdam ut,
+        vero voluptate.
+    </div>
+    <div>Ad aliquid amet debitis distinctio dolore dolorum ea excepturi facilis illum impedit laudantium minima natus
+        nihil nobis odio pariatur placeat provident qui repellat sint sit, tempora veniam veritatis voluptates
+        voluptatum.
+    </div>
+    <div>Amet at fugit minima. Autem consectetur, debitis distinctio doloremque enim inventore, iste minima odit
+        pariatur provident quo recusandae, rem sed tempore ullam veniam voluptate voluptates voluptatum? Amet eos
+        repellat suscipit!
+    </div>
+    <div>A aliquid consequuntur deleniti dolores eius eos excepturi facere facilis illo inventore ipsam laudantium
+        magni, minus modi nobis, odit, possimus quae quia quibusdam quos reiciendis repellat saepe soluta tempore
+        temporibus?
+    </div>
+    <div>A accusamus ad amet consequuntur cum, debitis deserunt dolore dolores explicabo facilis fugit impedit incidunt
+        ipsum labore laborum, magni mollitia nam nulla officia optio porro quisquam veritatis vero voluptate
+        voluptatibus?
+    </div>
+
+</div>
+
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js').sass('resources/assets/scss/app.scss', 'public/css', [
 
 ]);
+
+
+mix.disableNotifications();
+
+
